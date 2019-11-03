d7e7002b8cd5f3c64d6d61d022c5c2003d96c552
nguyễn minh <minh072710@gmail.com>
2019-11-03 19:46:06 +0700

    make it better

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ app.set("view engine", "ejs");
 app.set("views", "./Views");
 app.use(express.static("Public"));
 var moment=require('moment')
-
 //body-parser
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-
 //multer
 var multer = require("multer");
 var storage = multer.diskStorage({
@@ -21,7 +19,6 @@ var storage = multer.diskStorage({
     }
 
 });
-
 var Upload = multer({
     storage: storage,
     fileFilter: function (req, file, cb) {
@@ -36,47 +33,6 @@ var Upload = multer({
     }
 }).single("image");
 
-//express-session
-var session = require('express-session');
-app.use(session({ 
-    secret: 'aaaaaaaaa', 
-    cookie: { maxAge: 60*60 },//milisecond 10giay
-    resave:true,
-    saveUninitialized:true
-}));
-// var user=require("./Model/User");
-// app.get("/test",(req,res)=>{
-//     res.render("test");
-// })
-// app.post("/addtest",(req,res)=>{
-//    var us1=new user({
-//        firstname:req.body.firstname,
-//        lastname:req.body.lastname
-//    });
-//    console.log(us1);
-// })
-//multer-drive
-const { google } = require('googleapis');
-   
-    const multerDrive = require('multer-drive');
-    const auth = new google.auth.JWT({
-        email: 'minh072710@gmail.com',
-        key: 'Minh99824@',
-        scopes: ['https://www.googleapis.com/auth/drive'],
-    });
-    const upload = multer({
-        storage: multerDrive(auth),
-        // Rest of multer's options
-    });
-
- 
-    app.post('/upload', upload.single('file'), (req, res) => {
-        if(!req.file) {
-            return res.status(500).send('An error occurred while uploading your file');
-        }
-        res.status(200).send('Hurray! File was uploaded.');
-    });
- 
 //-----------------------------Connect FireStore----------------------------//
 const admin = require('firebase-admin');
 
@@ -91,7 +47,6 @@ const util = require('util');
 console.log(util.inspect('index.js', { showHidden: false, depth: null }));
 
 //-----------------------------Port Option----------------------------//
-
 app.listen(process.env.PORT||3000)
 
 //-----------------------------Route Main:Home----------------------------//
@@ -102,6 +57,17 @@ app.get("/",(req,res)=>{
 })
 
 //-----------------------------Route 1:USER----------------------------//
+//code generate id
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 app.get("/addgroupuser",(req,res)=>{
     res.render("Home",{
         pages:"Add_Group_User",
@@ -117,42 +83,18 @@ app.post("/addgroupuser",(req,res)=>{
     IdKids:[],
 
     });
-//cssds
-    // let list = [];
- 
-    // var db1 = db.collection('Tesst').get()
-    //     .then((snapshot) => {
-    //         snapshot.forEach((doc) => {
-    //             console.log(doc.id, '=>', doc.data());
-    //             list.push(doc.data());
-    //         });
 });
-
 app.get("/listgroupuser",(req,res)=>{
     res.render("Home",{
         pages:"Admin_Group_List"
     })
 });
-app.get("/adduser/",(req,res)=>{
+app.get("/adduser",(req,res)=>{
     res.render("Home",{
         pages:"Add_User"
     })
 
 });
-
-//code generate id
-function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-var jwt = require('jsonwebtoken');
-var secret="aaaaaaaaa";
-
 app.post("/adduser", function (req, res) {
     var id=makeid(20);
     
@@ -192,44 +134,40 @@ app.post("/adduser", function (req, res) {
 
 app.get("/listuser",(req,res)=>{
     let list = [];
-    let observer = db.collection('Admin').onSnapshot(async querySnapshot => {
-        querySnapshot.docChanges().forEach(change => {
-          if (change.type === 'added') {
-            console.log('New city: ', change.doc.data());
-            list.push(change.doc.data());
-
+    try{
+        let observer = db.collection('Admin').onSnapshot(querySnapshot => {
+            querySnapshot.docChanges().forEach(change => {
+              if (change.type === 'added') {
+                console.log('New city: ', change.doc.data());
+                list.push(change.doc.data());
+               
+              }else if (change.type === 'modified') {
+                console.log('Modified city: ', change.doc.data());
+                list.push(change.doc.data());
+               
+              }else if (change.type === 'removed') {
+                console.log('Removed city: ', change.doc.data());
+                return list.push(change.doc.data());
+              }
+            });
+            return res.render("Home", {
+                pages:"Admin_List",
+               
+                list: list,
+            })
         
-          }
-          if (change.type === 'modified') {
-            console.log('Modified city: ', change.doc.data());
-            return 1;
-            
-            
-          }
-          if (change.type === 'removed') {
-            console.log('Removed city: ', change.doc.data());
-            
-          }
-     
-        });
-        
-        res.render("Home", {
-            pages:"Admin_List",
-           
-            list: list,
         })
-    
-    })
+    }catch (err){
+        console.log(err)
+    }
 });
-
-
 app.get("/edituser",(req,res)=>{
     let list=[];
     var db1 = db.collection('Admin').where('id','=', req.query.id).get()
     .then((snapshot) => {
         snapshot.forEach((doc) => {
             // console.log(doc.id, '=>', doc.data());
-           list.push(doc.data())
+           return list.push(doc.data())
         //    console.log(list)
         });
         res.render("Home",{
@@ -242,79 +180,92 @@ app.get("/edituser",(req,res)=>{
    
 });
 app.post("/edituser/:id",(req,res)=>{
+    let ts =Date.now();
+    let date_ob=(new Date(ts)).toString();
+    let date=moment(date_ob).format("DD/MM/YY  hh:mm")
+    let cityRef = db.collection('Admin').doc(req.params.id);
+   
+    let updateMany = cityRef.update({
+        AcFullName: req.body.username,
+            Address: req.body.address,
+            Passwords: req.body.password,
+            Gender: req.body.gender,
+            Email: req.body.email,
+            Phone: req.body.txtEmpPhone,
+            Time:date,
+    });
+        
+    res.redirect("http://bigprojecta.herokuapp.com/listuser")
+  
+  
+  });
+app.get("/delete",(req,res)=>{
+    var db1 = db.collection('Admin').doc(req.query.id).delete();
+   // You aren't doing anything with data so no need for the return value
+upload(req,res,async (err)=>{
+    if(err){
+        console.log("eroor"+err);
+    }else{
+        await uploadToRemoteBucket(req.file.path)
+
+        // Delete the file like normal
+        await unlinkAsync(req.file.path)
+    
+        res.end("UPLOAD COMPLETED!")
+    }
+  
+})
+   res.redirect("http://bigprojecta.herokuapp.com/listuser")
+
+})
+//-----------------------------//Route 2:PRODUCT//----------------------------//
+app.get("/addgroupproduct",(req,res)=>{
+res.render("Home",{
+    pages:"Add_Group_Product"
+})
+});
+app.post("/addgroupproduct",(req,res)=>{
+var id=makeid(20);
+
+      
+            let ts =Date.now();
+            let date_ob=(new Date(ts)).toString();
+            let date=moment(date_ob).format("DD/MM/YY  hh:mm");
+            let docRef=db.collection("Categories").doc(req.body.name1).set({
+                catID:id,
+                img:req.body.downloadlink,
+                name:req.body.name,
+        
+                time:date
+            })
+            
+      
+
+
+});
+
+app.get("/test",(req,res)=>{
+res.render("Home",{
+    pages:"Test"
+})
+})
+
+
+
+
+         
+
+
+        
+
+        
 
 
     
   
     
         // [START update_document_many]
-        let ts =Date.now();
-        let date_ob=(new Date(ts)).toString();
-        let date=moment(date_ob).format("DD/MM/YY  hh:mm")
-        let cityRef = db.collection('Admin').doc(req.params.id);
-       
-        let updateMany = cityRef.update({
-            AcFullName: req.body.username,
-                Address: req.body.address,
-                Passwords: req.body.password,
-                Gender: req.body.gender,
-                Email: req.body.email,
-                Phone: req.body.txtEmpPhone,
-                Time:date,
-        });
-            
-        res.redirect("http://bigprojecta.herokuapp.com//listuser")
-      
-      
-      });
-app.get("/delete",(req,res)=>{
-        var db1 = db.collection('Admin').doc(req.query.id).delete();
-    
-       res.redirect("http://bigprojecta.herokuapp.com//listuser")
-  
-    })
-//-----------------------------//Route 2:PRODUCT//----------------------------//
-app.get("/addgroupproduct",(req,res)=>{
-    res.render("Home",{
-        pages:"Add_Group_Product"
-    })
-});
-app.post("/addgroupproduct",(req,res)=>{
-    var id=makeid(20);
-
-          
-                let ts =Date.now();
-                let date_ob=(new Date(ts)).toString();
-                let date=moment(date_ob).format("DD/MM/YY  hh:mm");
-                let docRef=db.collection("Categories").doc(req.body.name1).set({
-                    catID:id,
-                    img:req.body.downloadlink,
-                    name:req.body.name,
-            
-                    time:date
-                })
-                
-          
-  
-
-});
-
-app.get("/test",(req,res)=>{
-    res.render("Home",{
-        pages:"Test"
-    })
-})
-
-
-
-   
-             
-
-
-            
-   
-            
-
+     
 
 
 
